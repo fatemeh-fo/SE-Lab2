2cb30ed8f7c1cc9988dbd4fd5791ef08e4c96606
dorrin-sot <59933477+dorrin-sot@users.noreply.github.com>
2024-11-01 16:27:06 +0330

    Update TelegramMessageService.java

--- src/edu/sharif/selab/services/TelegramMessageService.java
+++ src/edu/sharif/selab/services/TelegramMessageService.java
@@ -1,7 +1,16 @@
 package edu.sharif.selab.services;
 
-import edu.sharif.selab.services.Messageservice;
+import edu.sharif.selab.models.EmailMessage;
+import edu.sharif.selab.models.SmsMessage;
 
-public class TelegramMessageService extends MessageService {
-  
+public class TelegramMessageService implements MessageService  {
+    @Override
+    public void sendSmsMessage(SmsMessage smsMessage) {
+        //Empty Body
+    }
+    
+    @Override
+    public void sendEmailMessage(EmailMessage emailMessage) {
+        //Empty Body
+    }
 }
