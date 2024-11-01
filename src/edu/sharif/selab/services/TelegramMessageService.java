55fb99c82b5429cf53b98ffdc3cf5c674edc983d
ElaheMrtz <62252751+elahemrtz@users.noreply.github.com>
2024-11-01 17:04:36 +0330

    Update TelegramMessageService.java

--- src/edu/sharif/selab/services/TelegramMessageService.java
+++ src/edu/sharif/selab/services/TelegramMessageService.java
@@ -2,8 +2,9 @@ package edu.sharif.selab.services;
 
 import edu.sharif.selab.models.EmailMessage;
 import edu.sharif.selab.models.SmsMessage;
+import edu.sharif.selab.models.TelegramMessage;
 
-public class TelegramMessageService implements MessageService  {
+public class TelegramMessageService implements MessageService{
     @Override
     public void sendSmsMessage(SmsMessage smsMessage) {
         //Empty Body
@@ -12,5 +13,23 @@ public class TelegramMessageService implements MessageService  {
     @Override
     public void sendEmailMessage(EmailMessage emailMessage) {
         //Empty Body
+    }
+
+    @Override
+    public void sendTelegramMessage(TelegramMessage telegramMessage) {
+        String sourceId = telegramMessage.getSourceId();
+        String targetId = telegramMessage.getTargetId();
+        String content = telegramMessage.getContent();
+
+        if (validateId(sourceId) && validateId(targetId)) {
+            System.out.println("Sending a SMS from " + sourceId + " to " + targetId + " with content : " + content);
+        } else {
+            throw new IllegalArgumentException("Id is Not Correct!");
+        }
+    }
+
+    public boolean validateId(String id) {
+        if (id.charAt(0) != '@') return false;
+        return true;
     }
 }
